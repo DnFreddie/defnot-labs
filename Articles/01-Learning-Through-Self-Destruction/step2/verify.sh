bd47a24b0f18c0c503ea9b9e505eaaac97005543
Dnfreddie <defnotfreddie@gmai.com>
2024-08-26 21:37:37 +0200

    Fixed validaiton

--- Articles/01-Learning-Through-Self-Destruction/step2/verify.sh
+++ Articles/01-Learning-Through-Self-Destruction/step2/verify.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-NICE_VALUE=$(ps -o nice= -p $(pgrep -n -f "nice_guy.sh"))
+
+PID=$(pgrep -n -f "nice_guy.sh")
+
+if [ -z "$PID" ]; then
+    echo "No process found matching 'nice_guy.sh'."
+    exit 1
+fi
+
+NICE_VALUE=$(ps -o nice= -p "$PID")
+
 if [ "$NICE_VALUE" -eq 19 ]; then
     echo "The nice value of the process is 19."
     exit 0
-
 else
-    echo "The nice value of the process is not 20. It is $NICE_VALUE."
+    echo "The nice value of the process is not 19. It is $NICE_VALUE."
     exit 1 
 fi
-
-
-
 
